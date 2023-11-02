29b51f623baa4c0a8783a66026d981bccc5fae43
Nurlan <nurlanea@code.edu.az>
2023-11-02 21:06:43 +0400

    upload

---
+++ Arrayde_cut_ededler/Program.cs
@@ -0,0 +1,14 @@
+﻿int numbers(int n, int m)
+{
+    int counter = 0;
+    while (n < m)
+    {
+        if (n % 2 == 0)
+        {
+            counter++;
+        }
+        n++;
+    }
+    return counter;
+}
+Console.WriteLine(numbers(1,10));
